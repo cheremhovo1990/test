886fae5108c8e6b5f04c210022f5e0de28e46deb
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-06-09 19:27:06 +0300

    added mix version

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,6 @@ const mix = require('laravel-mix');
 mix
     .setPublicPath('public/build')
     .setResourceRoot('/build/')
+    .version()
     .js('resources/js/app.js', 'js')
     .sass('resources/sass/app.scss', 'css');
