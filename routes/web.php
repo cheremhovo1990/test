ddfb1e0ab8d3743171ebaa8b2bb5126f5b658b0e
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-06-12 16:20:52 +0300

    added ssl

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/cabinet', 'Cabinet/HomeController@index')->name('cabinet');
+Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
