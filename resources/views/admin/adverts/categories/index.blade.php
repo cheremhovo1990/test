d43bb4aae2c33fb707396cd733282d01766554c4
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-07-14 14:12:08 +0300

    added category sorting

--- resources/views/admin/adverts/categories/index.blade.php
+++ resources/views/admin/adverts/categories/index.blade.php
@@ -9,6 +9,7 @@
         <tr>
             <th>Name</th>
             <th>Slug</th>
+            <th></th>
         </tr>
         </thead>
         <tbody>
@@ -17,6 +18,28 @@
                 <td>
                     @for ($i=0; $i < $category->depth; $i++) &mdash; @endfor
                     <a href="{{route('admin.adverts.categories.show')}}">{{$category->name}}</a>
+                </td>
+                <td>{{$category->slug}}</td>
+                <td>
+                    <div class="d-flex flex-row">
+                        <form method="POST" action="{{route('admin.adverts.categories.first', $category)}}"
+                              class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary">First</button>
+                        </form>
+                        <form method="POST" action="{{route('admin.adverts.categories.up', $category)}}" class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary">Up</button>
+                        </form>
+                        <form method="POST" action="{{route('admin.adverts.categories.down')}}" class="mr-1">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary">Down</button>
+                        </form>
+                        <form method="POST" action="{{route('admin.adverts.categories.last')}}">
+                            @csrf
+                            <button class="btn btn-sm btn-outline-primary">Last</button>
+                        </form>
+                    </div>
                 </td>
             </tr>
         @endforeach
