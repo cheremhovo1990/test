bfaa86170202d3482574510dd3c8d96f2ea15659
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-07-23 21:14:05 +0300

    added user phone

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Carbon\Carbon;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -16,9 +17,13 @@ use phpDocumentor\Reflection\Types\Self_;
  * @property $role
  * @property $name
  * @property $email
+ * @property $phone
+ * @property $phone_verified
  * @property $id
  * @property $password
  * @property $verify_token
+ * @property $phone_verify_token
+ * @property Carbon $phone_verify_token_expire
  */
 class User extends Authenticatable
 {
@@ -43,7 +48,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'last_name', 'email', 'password', 'status', 'verify_token', 'status', 'role'
+        'name', 'last_name', 'email', 'phone', 'password', 'status', 'verify_token', 'status', 'role'
     ];
 
     /**
@@ -62,6 +67,8 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_verified' => 'boolean',
+        'phone_verify_token_expire' => 'datetime',
     ];
 
     public static function register(string $name, string $email, string $password): self
@@ -122,8 +129,52 @@ class User extends Authenticatable
         $this->update(['role' => $role]);
     }
 
+    public function unverifyPhone()
+    {
+        $this->phone_verified = false;
+        $this->phone_verify_token = null;
+        $this->phone_verify_token_expire = null;
+        $this->saveOrFail();
+    }
+
+    public function requestPhoneVerification(Carbon $now): string
+    {
+        if (empty($this->phone)) {
+            throw new \DomainException('Phone number is empty.');
+        }
+        if (!empty($this->phone_verify_token) && $this->phone_verify_token_expire && $this->phone_verify_token_expire->gt($now)) {
+            throw new \DomainException('Token is already requested.');
+        }
+        $this->phone_verified = false;
+        $this->phone_verify_token = (string)random_int(10000, 99999);
+        $this->phone_verify_token_expire = $now->copy()->addSecond(300);
+        $this->saveOrFail();
+
+        return $this->phone_verify_token;
+    }
+
+    public function verifyPhone($token, Carbon $now)
+    {
+        if ($token !== $this->phone_verify_token) {
+            throw new \DomainException('Incorrect verify token.');
+        }
+
+        if ($this->phone_verify_token_expire->lt($now)) {
+            throw new \DomainException('Token is expired');
+        }
+        $this->phone_verified = true;
+        $this->phone_verify_token = null;
+        $this->phone_verify_token_expire = null;
+        $this->saveOrFail();
+    }
+
     public function isAdmin(): bool
     {
         return $this->role === self::ROLE_ADMIN;
+    }
+
+    public function isPhoneVerified(): bool
+    {
+        return $this->phone_verified;
     }
 }
---
+++ app/Http/Controllers/Cabinet/PhoneController.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cheremhovo1990
+ * Date: 23.07.19
+ * Time: 19:59
+ */
+declare(strict_types=1);
+
+
+namespace App\Http\Controllers\Cabinet;
+
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PhoneController extends Controller
+{
+    public function request(Request $request)
+    {
+        $user = Auth::user();
+
+        try {
+            $token = $user->requestPhoneVerification(Carbon::now());
+        } catch (\DomainException $e) {
+            $request->session()->flash('error', $e->getMessage());
+        }
+
+        return redirect()->route('cabinet.profile.phone');
+    }
+
+    public function form()
+    {
+        $user = Auth::user();
+
+        return view('cabinet.profile.phone', compact('user'));
+    }
+
+    public function verify(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required|string|max:255'
+        ]);
+
+        $user = Auth::user();
+
+        try {
+            $user->verifyPhone($request['token'], Carbon::now());
+        } catch (\DomainException $e) {
+            return redirect()->route('cabinet.profile.phone')->with('error', $e->getMessage());
+        }
+        return redirect()->route('cabinet.profile.home');
+    }
+}
