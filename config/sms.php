22828e2ababab2a5a3f094dda1ee89acbb58508a
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-07-26 22:18:22 +0300

    added sms array driver

--- config/sms.php
+++ config/sms.php
@@ -1,5 +1,15 @@
 <?php
 
+return [
+    'driver' => env('SMS_DRIVER', 'sms.ru'),
+
+    'drivers' => [
+        'sms.ru' => [
+            'app_id' => env('SMS_SMS_APP_ID'),
+            'url' => 'SMS_SMS_RU_URL',
+        ]
+    ]
+];
 
 return [
     'app_id' => env('SMS_RU_APP_ID'),
