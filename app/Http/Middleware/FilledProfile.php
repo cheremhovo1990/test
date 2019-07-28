7f9252fc6eb2b231781a83b471170834abffc727
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-07-28 14:36:04 +0300

    added filled profile middleware

---
+++ app/Http/Middleware/FilledProfile.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Http\Middleware;
+
+
+use Illuminate\Support\Facades\Auth;
+
+class FilledProfile
+{
+    public function handle($request, \Closure $next)
+    {
+        $user = Auth::user();
+
+        if (!$user->hasFilledProfile()) {
+            return redirect()
+                ->route('cabinet.profile.home')
+                ->with('error', 'Please fill your profile and verify your phone.');
+        }
+
+        return $next($request);
+    }
+}
