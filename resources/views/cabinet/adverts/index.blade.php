5f6e3531c33544a9de1528acb8176a0e1b509c1c
Ivan Kozhukhov <cheremhovo1990@yandex.ru>
2019-07-28 14:44:15 +0300

    fix

--- resources/views/cabinet/adverts/index.blade.php
+++ resources/views/cabinet/adverts/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <ul class="nav nav-tabs mb3">
-        <li class="nav-item"><a class="nav-item" href="{{route('cabinet.home')}}">Dashboard</a></li>
-        <li class="nav-item"><a class="nav-item active" href="{{route('cabinet.adverts.index')}}">Adverts</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{route('cabinet.home')}}">Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link active" href="{{route('cabinet.adverts.index')}}">Adverts</a></li>
         <li class="nav-item"><a class="nav-link" href="{{route('cabinet.profile.home')}}">Profile</a></li>
     </ul>
 @endsection
--- resources/views/cabinet/home.blade.php
+++ resources/views/cabinet/home.blade.php
@@ -3,7 +3,6 @@
 @section('breadcrumbs')
     <ul class="breadcrumb">
         <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="{{route('cabinet.adverts.index')}}">Adverts</a></li>
         <li class="breadcrumb-item active">Cabinet</li>
     </ul>
 @endsection
@@ -11,6 +10,7 @@
 @section('content')
     <ul class="nav nav-tabs mb-3">
         <li class="nav-item"><a class="nav-link active" href="{{route('cabinet.home')}}">Dashboard</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{route('cabinet.adverts.index')}}">Adverts</a></li>
         <li class="nav-item"><a class="nav-link" href="{{route('cabinet.profile.home')}}">Profile</a></li>
     </ul>
 @endsection
